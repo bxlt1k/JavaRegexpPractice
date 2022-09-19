f7e5e476904aae0269d871a99a09a1267211b4be
bxlt1k <a.pentyn@collabox.dev>
2022-09-19 14:15:33 +0300

    add first regex method

--- src/Regexp/Regexp.java
+++ src/Regexp/Regexp.java
@@ -1,5 +1,13 @@
 package Regexp;
 
-public class Regexp {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Regexp {
+    public static boolean time(String str) {
+        String timePattern = "(([0,1][0-9])|(2[0-3])):[0-5][0-9]";
+        Pattern pattern = Pattern.compile(timePattern);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
 }
